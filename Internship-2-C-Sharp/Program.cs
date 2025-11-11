e25efd9b622af9ec64a096e13e90bea9e612d9de
nduje <dujerancic@gmail.com>
2025-11-11 18:45:55 +0100

    Add initial project files

---
+++ Internship-2-C-Sharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Internship_2_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
