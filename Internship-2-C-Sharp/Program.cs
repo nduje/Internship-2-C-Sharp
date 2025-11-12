289b4d12b2a0a0ccdf2cfb1ab18814b80c1eee9e
nduje <dujerancic@gmail.com>
2025-11-12 18:35:04 +0100

    Add create new user functionality

--- Internship-2-C-Sharp/Program.cs
+++ Internship-2-C-Sharp/Program.cs
@@ -2,141 +2,10 @@
 {
     internal class Program
     {
+        static int users_number = 3;
+
         static void Main(string[] args)
         {
-            static void showMainMenu()
-            {
-                Console.WriteLine("1 - Korisnici");
-                Console.WriteLine("2 - Putovanja");
-                Console.WriteLine("0 - Izlaz iz aplikacije");
-                Console.WriteLine();
-
-                chooseFromMainMenu();
-            }
-
-            static void chooseFromMainMenu()
-            {
-                Console.Write("Odabir: ");
-                
-                if (int.TryParse(Console.ReadLine(), out int choice))
-                {
-                    Console.WriteLine();
-
-                    switch (choice)
-                    {
-                        case 0:
-                            return;
-                        case 1:
-                            showUsersMenu();
-                            break;
-                        case 2:
-                            showTripsMenu();
-                            break;
-                        default:
-                            Console.WriteLine("Unos nije valjan");
-                            break;
-
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Unos nije valjan");
-                }
-            }
-
-            static void showUsersMenu()
-            {
-                Console.WriteLine("1 - Unos novog korisnika");
-                Console.WriteLine("2 - Brisanje korisnika");
-                Console.WriteLine("3 - Uređivanje korisnika");
-                Console.WriteLine("4 - Pregled svih korisnika");
-                Console.WriteLine("0 - Povratak na glavni izbornik");
-                Console.WriteLine();
-
-                chooseFromUsersMenu();
-            }
-
-            static void chooseFromUsersMenu()
-            {
-                Console.Write("Odabir: ");
-
-                if (int.TryParse(Console.ReadLine(), out int choice))
-                {
-                    Console.WriteLine();
-
-                    switch (choice)
-                    {
-                        case 0:
-                            showMainMenu();
-                            break;
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        default:
-                            Console.WriteLine("Unos nije valjan");
-                            break;
-
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Unos nije valjan");
-                }
-            }
-
-            static void showTripsMenu()
-            {
-                Console.WriteLine("1 - Unos novog putovanja");
-                Console.WriteLine("2 - Brisanje putovanja");
-                Console.WriteLine("3 - Uređivanje postojećeg putovanja");
-                Console.WriteLine("4 - Pregled svih putovanja");
-                Console.WriteLine("5 - Izvještaji i analize");
-                Console.WriteLine("0 - Povratak na glavni izbornik");
-                Console.WriteLine();
-
-                chooseFromTripsMenu();
-            }
-
-            static void chooseFromTripsMenu()
-            {
-                Console.Write("Odabir: ");
-
-                if (int.TryParse(Console.ReadLine(), out int choice))
-                {
-                    Console.WriteLine();
-
-                    switch (choice)
-                    {
-                        case 0:
-                            showMainMenu();
-                            break;
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                        default:
-                            Console.WriteLine("Unos nije valjan");
-                            break;
-
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Unos nije valjan");
-                }
-            }
-
             var users = new Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>>()
             {
                 {1, new Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>
@@ -219,7 +88,204 @@
             };
 
             Console.WriteLine("APLIKACIJA ZA EVIDENCIJU GORIVA");
-            showMainMenu();
+            showMainMenu(users);
+
+            static void showMainMenu(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.WriteLine("1 - Korisnici");
+                Console.WriteLine("2 - Putovanja");
+                Console.WriteLine("0 - Izlaz iz aplikacije");
+                Console.WriteLine();
+
+                chooseFromMainMenu(users);
+            }
+
+            static void chooseFromMainMenu(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 0:
+                            return;
+                        case 1:
+                            showUsersMenu(users);
+                            break;
+                        case 2:
+                            showTripsMenu(users);
+                            break;
+                        default:
+                            Console.WriteLine("Unos nije valjan");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unos nije valjan");
+                }
+            }
+
+            static void showUsersMenu(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.WriteLine("1 - Unos novog korisnika");
+                Console.WriteLine("2 - Brisanje korisnika");
+                Console.WriteLine("3 - Uređivanje korisnika");
+                Console.WriteLine("4 - Pregled svih korisnika");
+                Console.WriteLine("0 - Povratak na glavni izbornik");
+                Console.WriteLine();
+
+                chooseFromUsersMenu(users);
+            }
+
+            static void chooseFromUsersMenu(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 0:
+                            showMainMenu(users);
+                            break;
+                        case 1:
+                            addNewUser(users);
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            Console.WriteLine("Unos nije valjan");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unos nije valjan");
+                }
+            }
+
+            static void showTripsMenu(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.WriteLine("1 - Unos novog putovanja");
+                Console.WriteLine("2 - Brisanje putovanja");
+                Console.WriteLine("3 - Uređivanje postojećeg putovanja");
+                Console.WriteLine("4 - Pregled svih putovanja");
+                Console.WriteLine("5 - Izvještaji i analize");
+                Console.WriteLine("0 - Povratak na glavni izbornik");
+                Console.WriteLine();
+
+                chooseFromTripsMenu(users);
+            }
+
+            static void chooseFromTripsMenu(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 0:
+                            showMainMenu(users);
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            Console.WriteLine("Unos nije valjan");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unos nije valjan");
+                }
+            }
+
+            static void addNewUser(Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>> users)
+            {
+                Console.WriteLine("UNOS NOVOG KORISNIKA");
+
+                Console.WriteLine();
+
+                string user_firstname, user_lastname, user_date_of_birth;
+
+                do
+                {
+                    Console.Write("Unesite ime: ");
+                    user_firstname = Console.ReadLine() ?? "";
+                } while (string.IsNullOrEmpty(user_firstname));
+
+                do
+                {
+                    Console.Write("Unesite prezime: ");
+                    user_lastname = Console.ReadLine() ?? "";
+                } while (string.IsNullOrEmpty(user_lastname));
+
+                do
+                {
+                    Console.Write("Unesite datum rođenja (YYYY-MM-DD): ");
+                    user_date_of_birth = Console.ReadLine() ?? "";
+                } while (string.IsNullOrEmpty(user_date_of_birth) || !isDateValid(user_date_of_birth));
+
+                Console.WriteLine();
+
+                users[users_number++] = new Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>
+                    (
+                        user_firstname,
+                        user_lastname,
+                        user_date_of_birth,
+                        new Dictionary<int, Tuple<string, double, double, double, double>>()
+                        {
+
+                        }
+                    );
+
+                Console.WriteLine("Dodan novi korisnik");
+                Console.WriteLine("");
+
+                showUsersMenu(users);
+            }
+
+            static bool isDateValid(string date)
+            {
+                if (date.Length != 10 && date[4] != '-' && date[7] != '-')
+                    return false;
+
+                string string_year = date.Substring(0, 4);
+                string string_month = date.Substring(5, 2);
+                string string_day = date.Substring(8, 2);
+
+                if (int.TryParse(string_year, out int year) && int.TryParse(string_month, out int month) && int.TryParse(string_day, out int day))
+                {
+                    if (year <= 2025 && month >= 1 && month <= 12 && day >= 1 && day <= 31)
+                        return true;
+                }
+
+                return false;
+            }
         }
     }
 }
