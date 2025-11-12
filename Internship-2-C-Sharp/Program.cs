7f63135061d317a7d771cbecb7178854e3c781f4
nduje <dujerancic@gmail.com>
2025-11-12 17:43:59 +0100

    Add initial console menu

--- Internship-2-C-Sharp/Program.cs
+++ Internship-2-C-Sharp/Program.cs
@@ -4,6 +4,139 @@
     {
         static void Main(string[] args)
         {
+            static void showMainMenu()
+            {
+                Console.WriteLine("1 - Korisnici");
+                Console.WriteLine("2 - Putovanja");
+                Console.WriteLine("0 - Izlaz iz aplikacije");
+                Console.WriteLine();
+
+                chooseFromMainMenu();
+            }
+
+            static void chooseFromMainMenu()
+            {
+                Console.Write("Odabir: ");
+                
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 0:
+                            return;
+                        case 1:
+                            showUsersMenu();
+                            break;
+                        case 2:
+                            showTripsMenu();
+                            break;
+                        default:
+                            Console.WriteLine("Unos nije valjan");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unos nije valjan");
+                }
+            }
+
+            static void showUsersMenu()
+            {
+                Console.WriteLine("1 - Unos novog korisnika");
+                Console.WriteLine("2 - Brisanje korisnika");
+                Console.WriteLine("3 - Uređivanje korisnika");
+                Console.WriteLine("4 - Pregled svih korisnika");
+                Console.WriteLine("0 - Povratak na glavni izbornik");
+                Console.WriteLine();
+
+                chooseFromUsersMenu();
+            }
+
+            static void chooseFromUsersMenu()
+            {
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 0:
+                            showMainMenu();
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            Console.WriteLine("Unos nije valjan");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unos nije valjan");
+                }
+            }
+
+            static void showTripsMenu()
+            {
+                Console.WriteLine("1 - Unos novog putovanja");
+                Console.WriteLine("2 - Brisanje putovanja");
+                Console.WriteLine("3 - Uređivanje postojećeg putovanja");
+                Console.WriteLine("4 - Pregled svih putovanja");
+                Console.WriteLine("5 - Izvještaji i analize");
+                Console.WriteLine("0 - Povratak na glavni izbornik");
+                Console.WriteLine();
+
+                chooseFromTripsMenu();
+            }
+
+            static void chooseFromTripsMenu()
+            {
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine();
+
+                    switch (choice)
+                    {
+                        case 0:
+                            showMainMenu();
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        default:
+                            Console.WriteLine("Unos nije valjan");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unos nije valjan");
+                }
+            }
+
             var users = new Dictionary<int, Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>>()
             {
                 {1, new Tuple<string, string, string, Dictionary<int, Tuple<string, double, double, double, double>>>
@@ -84,6 +217,9 @@
                     )
                 }
             };
+
+            Console.WriteLine("APLIKACIJA ZA EVIDENCIJU GORIVA");
+            showMainMenu();
         }
     }
 }
